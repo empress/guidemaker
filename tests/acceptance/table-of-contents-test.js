671c52d1676a51ac7e2310f82f9fdee4060f375a
Jen Weber <j@jenweber.me>
2019-10-18 17:44:23 +0200

    test for #25

---
+++ tests/acceptance/table-of-contents-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | table of contents', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('table of contents shows only pages for its own version', async function(assert) {
+    await visit('/v1.0.0/');
+    assert.equal(currentURL(), '/v1.0.0/');
+    assert.ok(document.querySelector('.toc-level-0').innerText.includes('Old Content'))
+    await visit('/release');
+    assert.equal(currentURL(), '/release');
+    assert.notOk(document.querySelector('.toc-level-0').innerText.includes('Old Content'))
+  });
+});
