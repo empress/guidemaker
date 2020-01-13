c2428ef1ac2bf04c40acc5747e4c3a0c55c4e48d
Jen Weber <j@jenweber.me>
2020-01-13 12:50:54 -0800

    Resolve ember-data:default-serializers deprecation

--- app/adapters/version.js
+++ app/adapters/version.js
@@ -1,18 +1,2 @@
-import DS from 'ember-data';
+export { default } from 'guidemaker/adapters/version';
 
-export default DS.JSONAPIAdapter.extend({
-  buildURL() {
-    let url = ['content', 'versions.json'];
-    let host = this.host;
-    let prefix = this.urlPrefix();
-
-    if (prefix) { url.unshift(prefix); }
-
-    url = url.join('/');
-    if (!host && url && url.charAt(0) !== '/') {
-      url = '/' + url;
-    }
-
-    return url;
-  },
-});
