b2e428aea8095e6b977cdd5327b367a4756daf3d
Chris Manson <chris@manson.ie>
2020-01-28 13:53:01 +0100

    simplfy environment.js rewrite

--- blueprints/guidemaker/index.js
+++ blueprints/guidemaker/index.js
@@ -1,10 +1,7 @@
 /* eslint-env node */
 
-const recast = require('recast');
 const { readFileSync, writeFileSync } = require('fs');
-const { applyBuildConfig } = require('empress-blueprint-helpers');
-
-const builders = recast.types.builders;
+const { applyBuildConfig, applyConfig } = require('empress-blueprint-helpers');
 
 module.exports = {
   description: 'The default blueprint for guidemaker.',
@@ -31,56 +28,14 @@ module.exports = {
       extensions: ['js', 'css', 'map'],
     })
 
-    const config = readFileSync('./config/environment.js');
-    const configAst = recast.parse(config);
-
-    recast.visit(configAst, {
-      visitVariableDeclaration: function (path) {
-        var node = path.node;
-
-        const env = node.declarations.find(declaration => declaration.id.name === 'ENV');
-
-        if (env) {
-          let locationType = env.init.properties.find(property => property.key.name === 'locationType');
-
-          if(locationType) {
-            locationType.value = builders.literal('trailing-history');
-          }
-
-          let historySupportMiddleware = env.init.properties.find(property => property.key.name === 'historySupportMiddleware');
-
-          if(historySupportMiddleware) {
-            historySupportMiddleware.value = builders.literal(true);
-          } else {
-            historySupportMiddleware = builders.property(
-              'init',
-              builders.identifier('historySupportMiddleware'),
-              builders.literal(true)
-            );
-
-            // insert just after the locationType
-            env.init.properties.splice(env.init.properties.indexOf(locationType) + 1, 0, historySupportMiddleware);
-          }
-
-          let guidemaker = env.init.properties.find(property => property.key.value === 'guidemaker');
-
-          if(!guidemaker) {
-            guidemaker = builders.property(
-              'init',
-              builders.identifier('guidemaker'),
-              builders.objectExpression([
-                builders.property('init', builders.identifier('title'), builders.literal('Guidemaker Docs'))
-              ])
-            )
-            env.init.properties.push(guidemaker);
-          }
-        }
-
-        this.traverse(path);
-      }
+    applyConfig(this.project, 'guidemaker', {
+      title: 'Guidemaker Docs',
+      description: 'Guides - Built with Guidemaker',
     });
 
-    writeFileSync('./config/environment.js', recast.print(configAst, { tabWidth: 2, quote: 'single' }).code);
+    applyConfig(this.project, 'locationType', 'trailing-history', true);
+
+    applyConfig(this.project, 'historySupportMiddleware', true, true);
 
     const package = JSON.parse(readFileSync('./package.json'));
     package.scripts = Object.assign(package.scripts, {
