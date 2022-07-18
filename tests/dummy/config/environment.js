a8066bb919c8e054c2cee1cce5df1ca3c1dcf7e1
Chris Manson <chris@manson.ie>
2022-07-18 14:48:06 +0100

    update to ember 3.28 using ember-cli-update

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(environment) {
+module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'dummy',
     environment,
@@ -14,8 +14,8 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
     APP: {
@@ -23,11 +23,8 @@ module.exports = function(environment) {
       // when it is created
     },
 
-    'ember-meta': {
-      description: 'Guides - Built with Guidemaker'
-    },
-
     guidemaker: {
+      description: 'Guides - Built with Guidemaker',
       title: 'Guidemaker Docs'
     }
   };
