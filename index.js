544329c97cd70890e05c15e3b227a0ff74fa4f6f
Ralph Mack <ralphmack@comcast.net>
2023-09-30 12:46:40 -0400

    Removed included() method, which only added ember-prism options. Now we count on ember-showdown-prism to do that.

--- index.js
+++ index.js
@@ -287,29 +287,4 @@ You should move it into the 'guides' folder.
       'ember-collapsible-panel': config['ember-collapsible-panel'] || {}
     }
   },
-
-  included(app) {
-    this._super.included.apply(this, arguments)
-
-    if(!app.options['ember-prism']) {
-      app.options['ember-prism'] = {
-        theme: 'okaidia',
-
-        components: [
-          'apacheconf',
-          'bash',
-          'css',
-          'handlebars',
-          'http',
-          'javascript',
-          'json',
-          'markup-templating',
-          'ruby',
-          'scss'
-        ],
-
-        plugins: ['line-numbers', 'normalize-whitespace']
-      }
-    }
-  },
 };
