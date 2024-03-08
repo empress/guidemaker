6b870270701661cce3b6afbe3fed4193df204b02
Chris Manson <chris@manson.ie>
2024-03-08 12:00:56 +0000

    fix tests

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -12,6 +12,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+            'ember-cli': '~4.12.0',
           },
         },
       },
