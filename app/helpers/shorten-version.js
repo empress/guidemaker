eee307fab81d78a1e63ab85904eef8737edcbfdd
Chris Manson <chris@manson.ie>
2024-03-08 19:45:13 +0000

    fix the location of the shorten-version helper
    this will allow people to import it from guidemaker

--- app/helpers/shorten-version.js
+++ app/helpers/shorten-version.js
@@ -1,8 +1,1 @@
-/* eslint-disable prettier/prettier */
-import { helper } from '@ember/component/helper';
-
-export function shortenVersion([version='']) {
-  return version.slice(version.indexOf('v') + 1 || 0, version.lastIndexOf('.') === version.indexOf('.') ? version.length : version.lastIndexOf('.'));
-}
-
-export default helper(shortenVersion);
+export { default } from 'guidemaker/helpers/shorten-version';
